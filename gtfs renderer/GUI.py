c59b3a23f43d67ed9cf678b2c8879eadee80084e
kallejre <kallejre@yahoo.com>
2021-09-09 23:36:58 +0300

    GTFS basic UI is in place

--- gtfs renderer/GUI.py
+++ gtfs renderer/GUI.py
@@ -46,40 +46,21 @@ def getInputBoxValue():
 	userInput = tInput.get()
 	return userInput
 
-class 
 
-    listbox = Listbox(root)
-      
-    # Adding Listbox to the left
-    # side of root window
-    listbox.pack(side = LEFT, fill = BOTH)
-      
-    # Creating a Scrollbar and 
-    # attaching it to root window
-    scrollbar = Scrollbar(root)
-      
-    # Adding Scrollbar to the right
-    # side of root window
-    scrollbar.pack(side = RIGHT, fill = BOTH)
-      
-    # Insert elements into the listbox
-    for values in range(100):
-        listbox.insert(END, values)
+class Scrollbox(Frame):
+    def __init__(self, root, **kwargs):
+        super().__init__(root)
+        self.listbox = Listbox(self, **kwargs)
+        self.listbox.pack(side = LEFT, fill = BOTH)
+        self.scrollbar = Scrollbar(self)
+        self.scrollbar.pack(side = RIGHT, fill = BOTH)
           
-    # Attaching Listbox to Scrollbar
-    # Since we need to have a vertical 
-    # scroll we use yscrollcommand
-    listbox.config(yscrollcommand = scrollbar.set)
-      
-    # setting scrollbar command parameter 
-    # to listbox.yview method its yview because
-    # we need to have a vertical view
-    scrollbar.config(command = listbox.yview)
-  
-  
-  
-
-
+        # Insert elements into the listbox
+        #for values in range(100):
+        #    self.listbox.insert(END, values)
+        
+        self.listbox.config(yscrollcommand = self.scrollbar.set)
+        self.scrollbar.config(command = self.listbox.yview)
 
 
 root = Tk()
@@ -91,74 +72,66 @@ root.title('Hello, I\'m the main window')
 
 
 # This is the section of code which creates a listbox
-listBoxOne=Listbox(root, width=0, height=0)
-listBoxOne.insert('0', 'Walnuts')
-listBoxOne.insert('1', 'corn')
-listBoxOne.insert('2', 'Italian bread')
-listBoxOne.insert('3', 'chimichanga')
-listBoxOne.insert('4', 'jambalaya')
-listBoxOne.insert('5', 'crab')
-listBoxOne.insert('6', 'Milk')
-listBoxOne.insert('7', 'Lamb')
-listBoxOne.insert('8', 'BBQ')
-listBoxOne.insert('9', 'babaganoosh')
-listBoxOne.place(x=41, y=111)
+listBoxOne=Scrollbox(root, width=0, height=0, selectmode = "extended")
+listBoxOne.listbox.insert('0', 'Walnuts')
+listBoxOne.listbox.insert('1', 'corn')
+listBoxOne.listbox.insert('2', 'Italian bread')
+listBoxOne.listbox.insert('3', 'chimichanga')
+listBoxOne.listbox.insert('4', 'jambalaya')
+listBoxOne.listbox.insert('5', 'crab')
+listBoxOne.listbox.insert('6', 'Milk')
+listBoxOne.listbox.insert('7', 'Lamb')
+listBoxOne.listbox.insert('8', 'BBQ')
+listBoxOne.listbox.insert('9', 'babaganoosh')
+listBoxOne.grid(row=0, column=0, rowspan=8, sticky="NSEW")
 
 
 # This is the section of code which creates a combo box
 comboOneTwoPunch= ttk.Combobox(root, values=['chocolate', 'beer', 'Englishmuffins', 'duck', 'Italian bread'], width=10)
-comboOneTwoPunch.place(x=181, y=161)
+comboOneTwoPunch.grid(row=7, column=1, rowspan=1)
 comboOneTwoPunch.current(1)
 
 
-# This is the section of code which creates a button
-Button(root, text='>>', command=btnClickFunction).place(x=244, y=256)
-
-
-# This is the section of code which creates a button
-Button(root, text='>', command=btnClickFunction).place(x=266, y=305)
-
-
 # This is the section of code which creates a listbox
-listBoxTwo=Listbox(root, width=0, height=0)
-listBoxTwo.insert('0', 'French dip')
-listBoxTwo.insert('1', 'bluefish')
-listBoxTwo.insert('2', 'Moose')
-listBoxTwo.insert('3', 'bread')
-listBoxTwo.insert('4', 'eggs')
-listBoxTwo.insert('5', 'kabobs')
-listBoxTwo.insert('6', 'Linguine')
-listBoxTwo.insert('7', 'Noodles')
-listBoxTwo.insert('8', 'hamburger')
-listBoxTwo.insert('9', 'Indian food')
-listBoxTwo.insert('10', 'Guancamole')
-listBoxTwo.insert('11', 'dips')
-listBoxTwo.insert('12', 'antelope')
-listBoxTwo.insert('13', 'grapes')
-listBoxTwo.insert('14', 'goose')
-listBoxTwo.place(x=286, y=8)
+listBoxTwo=Scrollbox(root, width=0, height=15, selectmode = "extended")
+listBoxTwo.listbox.insert('0', 'French dip')
+listBoxTwo.listbox.insert('1', 'bluefish')
+listBoxTwo.listbox.insert('2', 'Moose')
+listBoxTwo.listbox.insert('3', 'bread')
+listBoxTwo.listbox.insert('4', 'eggs')
+listBoxTwo.listbox.insert('6', 'Linguine')
+listBoxTwo.listbox.insert('7', 'Noodles')
+listBoxTwo.listbox.insert('8', 'hamburger')
+listBoxTwo.listbox.insert('9', 'Indian food')
+listBoxTwo.listbox.insert('10', 'Guancamole')
+listBoxTwo.listbox.insert('11', 'dips')
+listBoxTwo.listbox.insert('12', 'antelope')
+listBoxTwo.listbox.insert('13', 'grapes')
+listBoxTwo.listbox.insert('14', 'goose')
+listBoxTwo.listbox.insert('13', 'grapes')
+listBoxTwo.listbox.insert('14', 'goose')
+listBoxTwo.listbox.insert('13', 'grapes')
+listBoxTwo.listbox.insert('14', 'goose')
+listBoxTwo.grid(row=0, column=2, rowspan=8, sticky="NSEW")
 
 
 # This is the section of code which creates a button
-Button(root, text='<',command=btnClickFunction).place(x=206, y=218)
+X = 1
+Y = 1
+Button(root, text='>>', command=btnClickFunction).grid(column=X, row=Y*1, rowspan=1, sticky="NSEW")
+Button(root, text='>', command=btnClickFunction).grid(column=X, row=Y*2, rowspan=1, sticky="NSEW")
+Label( root, text='0 selected').grid(column=X, row=Y*3, rowspan=1, sticky="NSEW")
+Button(root, text='<',command=btnClickFunction).grid(column=X, row=Y*4, rowspan=1, sticky="NSEW")
+Button(root, text='<<', command=btnClickFunction).grid(column=X, row=Y*5, rowspan=1, sticky="NSEW")
 
-
-# This is the section of code which creates a button
-Button(root, text='<<', command=btnClickFunction).place(x=194, y=315)
-
-
-# This is the section of code which creates a text input box
-tInput=Entry(root)
-tInput.place(x=60, y=392)
-
+url_box=Entry(root)
+url_box.grid(row=9, column=0, columnspan=3, sticky="NSEW")
 
 # First, we create a canvas to put the picture on
 worthAThousandWords= Canvas(root, height=256, width=256)
 # Then, we actually create the image file to use (it has to be a *.gif)
-picture_file = PhotoImage(file = '')  # <-- you will have to copy-paste the filepath here, for example 'C:\Desktop\pic.gif'
+picture_file = PhotoImage(master=root, file = 'sample.png')  # <-- you will have to copy-paste the filepath here, for example 'C:\Desktop\pic.gif'
 # Finally, we create the image on the canvas and then place it onto the main window
 worthAThousandWords.create_image(256, 0, anchor=NE, image=picture_file)
-worthAThousandWords.place(x=209, y=366)
-
-
+worthAThousandWords.grid(row=11, column=0, columnspan=4)
 root.mainloop()
